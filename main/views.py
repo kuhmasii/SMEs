7c9348ad1e875a6cfc43692faadb2c4edf66ad0e
kuhmasii <olaisaiah54@gmail.com>
2023-03-14 07:52:44 +0100

    added new functionalities and cleared dirty codes

--- main/views.py
+++ main/views.py
@@ -3,17 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-	return render(request,'index.html')
+	return render(request,'main/index.html')
 
 def about(request):
-	return render(request, 'about.html')
-
+	return render(request, 'main/about.html')
 
 def enterprise(request):
-	return render(request,'enterprise.html')
+	return render(request,'main/enterprise.html')
 
 def medium(request):
-	return render(request, 'medium.html')
+	return render(request, 'main/medium.html')
 
 def small(request):
-	return render(request, 'small.html')
+	return render(request, 'main/small.html')
+
+def business_plan(request):
+	return render(request, 'main/business.html')
+
+
+def service(request):
+	return render(request, 'main/services.html')
+
+
+def detail_small(request):
+	return render(request, 'main/detail.html')
+
+def detail_medium(request):
+	return render(request, 'main/detail.html')
+
+def detail_enterprise(request):
+	return render(request, 'main/detail.html')
+
+
